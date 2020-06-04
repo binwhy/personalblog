ac56578df6d32e4d64eb86c661411ccb675e23c6
binwhy <942391077@qq.com>
2020-06-04 22:59:22 +0800

    day4-home主体框架搭建与顶部搜索-部分完成

---
+++ src/components/content/topSearch/TopSearch.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="top-search">
+    <div class="search">
+      <el-input
+        placeholder="请输入关键字搜索"
+        v-model="input">
+        <i slot="suffix" class="el-input__icon el-icon-search"></i>
+      </el-input>
+    </div>
+    <div class="right-part">
+      <el-popover
+        :visible-arrow="false"
+        placement="bottom"
+        title="随心所语"
+        width="320"
+        trigger="click"
+        content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+        <i class="el-icon-chat-line-round" slot="reference"></i>
+      </el-popover>
+      <el-popover
+        :popper-options="{ boundariesElement: 'viewport', removeOnDestroy: true }"
+        :visible-arrow="false"
+        placement="bottom"
+        title="标题"
+        width="200"
+        trigger="click"
+        content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+        <i class="el-icon-user" slot="reference"></i>
+      </el-popover>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TopSearch",
+    data() {
+      return {
+        input: '',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .top-search {
+    position: absolute;
+    top: 0;
+    background-color: #f9f9f9;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    box-shadow:0px 0px 3px #f2f2f2;
+  }
+
+  .search {
+    display: flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    width: 180px;
+    height: 50px;
+    margin-left: 20px;
+  }
+
+  .right-part {
+    position: absolute;
+    right: 0;
+    width: 220px;
+    height: 50px;
+    padding: 5px 10px;
+    display: flex;
+    flex-direction:row;
+    justify-content:space-around;
+    align-items: center;
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,55 @@
 <template>
   <div id="home" class="all-main-content">
-    <h2>Home {{message}}</h2>
+    <top-search/>
+    <scroll
+      ref="scroll"
+      class="content"
+      :probeType="3"
+      :pullUpLoad=true
+    >
+      <div class="main">
+        <div class="main-left">
+          <div class="home-header">
+            <h1>Baymax2dm's Blog</h1>
+            <small>赠人玫瑰，手有余香。</small>
+          </div>
+          <div class="home-body">
+            <h2>Home {{message}}</h2>
+
+          </div>
+        </div>
+
+        <div class="main-right">
+
+        </div>
+      </div>
+      <div class="bottom">
+        <span>© 2020 Copyright</span>
+      </div>
+
+    </scroll>
   </div>
 </template>
 
 <script>
-
+  import Scroll from 'components/common/scroll/Scroll';//滚动
+  import TopSearch from "components/content/topSearch/TopSearch";
   export default {
     name: "Home",
     components: {
-
+      Scroll,
+      TopSearch
     },
     data() {
       return {
-        message: "hi 我的博客-day1"
+        message: "hi 我的博客-day4"
       }
+    },
+    activated() {
+      // let header = document.querySelector('.home-header');
+      // let body = document.querySelector('.home-header');
+      // let content = document.querySelector('.content');
+      // content.height = header.offsetHeight + body.offsetHeight;
     },
     methods: {
 
@@ -23,5 +58,72 @@
 </script>
 
 <style scoped>
+  .content {
+    position: absolute;
+    top: 50px;
+    bottom: 0;
+    width: 100%;
+    height: calc(100% -50px);
+    display: flex;
+    flex-direction:column;
+    overflow: hidden;
+    background-color: #f1f3f4;
+  }
+
+  .main{
+    position: relative;
+    top: 3px;
+    display: flex;
+    flex-direction:row;
+  }
+
+  .bottom {
+    position: relative;
+    bottom: 0;
+    height: 50px;
+    background-color: #222222;
+    padding: 15px;
+    font-size: 14px;
+    text-align: center;
+  }
+
+  .main-left {
+    position: relative;
+    flex: 1;
+    /*display: flex;*/
+    /*flex-direction:column;*/
+  }
+
+  .main-right {
+    position: relative;
+    width: 220px;
+    padding: 0 20px;
+    background-color: #f9f9f9;
+  }
+
+  .home-header {
+    height: 111px;
+    padding: 20px;
+    background-color: #f9f9f9;
+  }
+
+  .home-header h1{
+    font-size: 36px;
+    height: 51px;
+    margin: 0;
+  }
+
+  .home-header small{
+    font-size: 13px;
+    height: 17px;
+    margin: 0;
+    color: #98A6AD;
+  }
+
+  .home-body {
+    padding: 20px;
+    height: 1000px;
+    background-color: #f1f3f4;
+  }
 
 </style>
